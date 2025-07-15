248f9767fd24c05332c13c97ca7d1dfb58e213c1
Zinfu123 <Ninfu1@gmail.com>
2025-07-14 22:38:55 -0400

    Updated pokemonCard to properly change the background color of the card based on the type of the pokemon.

--- resources/js/components/pokemon/PokemonCard.vue
+++ resources/js/components/pokemon/PokemonCard.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { ref, onMounted } from 'vue';
+
 
 interface Pokemon {
     name: string;
@@ -13,37 +13,34 @@ interface props {
     pokemon: Pokemon;
 }
 
-const isDark = ref(false);
-
-onMounted(() => {
-    // 1. Check local storage
-    if (localStorage.getItem('darkMode') === 'true') {
-      isDark.value = true;
-    }
-
-    // 2. Check for "dark" class on the HTML or body element
-    if (document.documentElement.classList.contains('dark')) {
-      isDark.value = true;
-    }
-
-    // Alternatively, if you're using a specific library or custom solution,
-    // you might have a different way to access the current dark mode state.
-  });
-
 const props = defineProps<props>();
 
-const typeColor = () => {
-    if (isDark.value === true) {
-        return `type-${props.pokemon.type1.toLowerCase()}-dark`;
-    }
-    else {
-        return `type-${props.pokemon.type1.toLowerCase()}`;
-    }
-};
+const type = 'bg-' + props.pokemon.type1.toLowerCase() + 'type';
+
+console.log(type);
 
 </script>
 <template>
-    <Card :class="typeColor()">
+    <!-- Change Card Background Color -->
+    <Card :class="['base-class', 'bg-normaltype', 
+    { 
+        'bg-rocktype': props.pokemon.type1 === 'Rock', 
+        'bg-watertype': props.pokemon.type1 === 'Water', 
+        'bg-firetype': props.pokemon.type1 === 'Fire', 
+        'bg-grasstype': props.pokemon.type1 === 'Grass', 
+        'bg-electrictype': props.pokemon.type1 === 'Electric', 
+        'bg-poisontype': props.pokemon.type1 === 'Poison', 
+        'bg-groundtype': props.pokemon.type1 === 'Ground', 
+        'bg-psychictype': props.pokemon.type1 === 'Psychic', 
+        'bg-icetype': props.pokemon.type1 === 'Ice', 
+        'bg-dragontype': props.pokemon.type1 === 'Dragon', 
+        'bg-fairytype': props.pokemon.type1 === 'Fairy', 
+        'bg-darktype': props.pokemon.type1 === 'Dark', 
+        'bg-steeltype': props.pokemon.type1 === 'Steel', 
+        'bg-flyingtype': props.pokemon.type1 === 'Flying', 
+        'bg-fightingtype': props.pokemon.type1 === 'Fighting', 
+        'bg-bugtype': props.pokemon.type1 === 'Bug', 
+        'bg-ghosttype': props.pokemon.type1 === 'Ghost',  }]">
         <CardHeader>
             <CardTitle :class="`text-center text-2xl font-bold capitalize`">
                 {{ props.pokemon.name }}
@@ -51,14 +48,13 @@ const typeColor = () => {
         </CardHeader>
         <CardContent class="flex flex-col items-center justify-center relative">
             <div class="z-10">
-                <div class="diagonal-line"></div>
             </div>
-            <div class="z-20">
+            <div class="z-20 flex items-center justify-center">
             <img :src="props.pokemon.sprite_url" :alt="props.pokemon.name" />
-                <span class="rounded bg-muted px-2 py-1 text-xs">
+                <span class="rounded bg-muted px-2 py-1 text-xs text-center">
                     {{ props.pokemon.type1 }}
                 </span>
-                <span v-if="props.pokemon.type2" class="rounded bg-muted px-2 py-1 text-xs">
+                <span v-if="props.pokemon.type2" class="rounded bg-muted px-2 py-1 text-xs text-center">
                     {{ props.pokemon.type2 }}
                 </span>
             </div>
@@ -67,148 +63,4 @@ const typeColor = () => {
 </template>
 
 <style>
-.type-normal {
-    background-color: hsl(0 0% 66.7%);
-}
-
-.type-normal-dark {
-    background-color: hsl(0, 0%, 81%);
-}
-
-.type-fire {
-    background-color: hsl(12, 85%, 43%);
-}
-
-.type-fire-dark {
-    background-color: hsl(12, 89%, 44%);
-}
-
-.type-water {
-    background-color: hsl(207 90% 54%);
-}
-
-.type-water-dark {
-    background-color: hsl(207 90% 34%);
-}
-
-.type-electric {
-    background-color: hsl(56 94% 57%);
-}
-
-.type-electric-dark {
-    background-color: hsl(56 94% 37%);
-}
-
-.type-grass {
-    background-color: hsl(123, 63%, 36%, 0.877);
-}
-
-.type-grass-dark {
-    background-color: hsl(120, 66%, 21%);
-}
-
-.type-ice {
-    background-color: hsl(195 100% 81%);
-}
-
-.type-ice-dark {
-    background-color: hsl(195 100% 61%);
-}
-
-.type-fighting {
-    background-color: hsl(19, 80%, 49%);
-}
-
-.type-fighting-dark {
-    background-color: hsl(19, 80%, 49%);
-}
-
-.type-poison {
-    background-color: hsl(283 77% 57%);
-}
-
-.type-poison-dark {
-    background-color: hsl(283 77% 37%);
-}
-
-.type-ground {
-    background-color: hsl(45, 71%, 27%);
-}
-
-.type-ground-dark {
-    background-color: hsl(45, 67%, 25%);
-}
-
-.type-psychic {
-    background-color: hsl(323 100% 57%);
-}
-
-.type-psychic-dark {
-    background-color: hsl(323 100% 37%);
-}
-
-.type-bug {
-    background-color: hsl(91, 100%, 63%);
-}
-
-.type-bug-dark {
-    background-color: hsl(91, 100%, 63%);
-}
-
-.type-rock {
-    background-color: hsl(29, 24%, 31%);
-}
-
-.type-rock-dark {
-    background-color: hsl(29, 24%, 31%);
-}
-
-.type-ghost {
-    background-color: hsl(270, 100%, 78%);
-}
-
-.type-ghost-dark {
-    background-color: hsl(270, 100%, 78%);
-}
-
-.type-dragon {
-    background-color: hsl(256, 78%, 57%);
-}
-
-.type-dragon-dark {
-    background-color: hsl(256, 78%, 57%);
-}
-
-.type-dark {
-    background-color: hsl(37, 31%, 15%);
-}
-
-.type-dark-dark {
-    background-color: hsl(37, 31%, 15%);
-}
-
-.type-steel {
-    background-color: hsl(240, 1%, 32%);
-}
-
-.type-steel-dark {
-    background-color: hsl(240, 1%, 32%);
-}
-
-.type-fairy {
-    background-color: hsl(310, 100%, 76%);
-}
-
-.type-fairy-dark {
-    background-color: hsl(310, 100%, 76%);
-}
-
-.diagonal-line{
-    width: 100px;
-    height: 2px;
-    background-color: black;
-    transform: rotate(45deg);
-    transform-origin: top left;
-}
-
 </style>
--- resources/js/pages/pokedex/PokedexIndex.vue
+++ resources/js/pages/pokedex/PokedexIndex.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
 import PokemonCard from '@/components/pokemon/PokemonCard.vue';
+import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
+import AppLayout from '@/layouts/AppLayout.vue';
 
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Dashboard',
+        href: '/dashboard',
+    },
+    {
+        title: 'Pokedex',
+        href: '/pokedex',
+    },
+];
 interface Pokemon {
     id: number;
     name: string;
@@ -20,7 +32,10 @@ const props = defineProps<props>();
 
 <template>
     <Head title="Pokedex" />
+    <AppLayout :breadcrumbs="breadcrumbs">
+        
     <div class="grid grid-cols-6 gap-3">
         <PokemonCard v-for="pokemon in props.pokemon" :key="pokemon.id" :pokemon="pokemon" />
     </div>
+</AppLayout>
 </template>
