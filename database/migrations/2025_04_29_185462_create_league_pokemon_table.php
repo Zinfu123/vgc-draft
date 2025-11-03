ddcf0f105af7e629893fc48770043c786c401f10
Zinfu123 <Ninfu1@gmail.com>
2025-11-03 11:48:13 -0500

    removed drafted_by fk from the leaguepokemon migration to use a secondary migration for it.

--- database/migrations/2025_04_29_185462_create_league_pokemon_table.php
+++ database/migrations/2025_04_29_185462_create_league_pokemon_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('name');
             $table->integer('cost');
             $table->boolean('is_drafted')->default(false);
-            $table->foreignId('drafted_by')->nullable()->constrained('teams');
             $table->timestamps();
         });
     }
