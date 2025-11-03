0a7e52f64a3ef73d93810b21bd574b7efff90e5f
Zinfu123 <Ninfu1@gmail.com>
2025-11-03 12:15:08 -0500

    fixing migrations

--- database/migrations/2025_04_29_185803_create_draftpicks_table.php
+++ database/migrations/2025_04_29_185803_create_draftpicks_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('draft_picks', function (Blueprint $table) {
             $table->id();
             $table->foreignId('draft_id')->constrained('drafts')->onDelete('cascade');
-            $table->foreignId('team_id')->constrained('teams')->onDelete('cascade');
             $table->foreignId('league_pokemon_id')->constrained('league_pokemon')->onDelete('cascade');
             $table->integer('round_number');
             $table->integer('pick_number');
